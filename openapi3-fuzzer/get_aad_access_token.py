87d4f87e3397108573085f5d99f5b637b0e1302d
Jorrit <j.folmer@vwt.digital>
2019-12-15 22:30:14 +0100

    Add script to obtain AAD access token

---
+++ openapi3-fuzzer/get_aad_access_token.py
@@ -0,0 +1,32 @@
+import json
+import adal
+import argparse
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('resource', type=str, help='AAD resource, e.g. https://vwt-d-myresource')
+  parser.add_argument('tenant', type=str, help='AAD tenant, e.g. beefbeef-beef-beef-beefbeefbeef')
+  parser.add_argument('client_id', type=str, help='AAD client id, e.g. beefbeef-beef-beef-beefbeefbeef"')
+  parser.add_argument('client_secret', type=str, help='AAD client secret, e.g. MyVeryLongSecrett!!!11"')
+  args = parser.parse_args()
+
+params = {
+    "resource": args.resource,
+    "tenant" : args.tenant,
+    "authorityHostUrl" : "https://login.microsoftonline.com",
+    "clientId" : args.client_id,
+    "clientSecret" : args.client_secret
+}
+
+authority_url = (params['authorityHostUrl'] + '/' + params['tenant'])
+
+context = adal.AuthenticationContext(
+    authority_url, validate_authority=params['tenant'] != 'adfs',
+    )
+
+token = context.acquire_token_with_client_credentials(
+    params['resource'],
+    params['clientId'],
+    params['clientSecret'])
+
+print("{} {}".format(token.tokenType, token.accessToken))
