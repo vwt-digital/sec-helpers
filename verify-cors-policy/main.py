3d62b863f49bbed49b993006fe441a5e2937b61f
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-02-28 11:33:02 +0100

    Added cors policy check DAT-2065

---
+++ verify-cors-policy/main.py
@@ -0,0 +1,34 @@
+import requests
+import argparse
+import sys
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('domain', type=str, help='Secured API url')
+    parser.add_argument('type', type=str, help='type')
+    parser.add_argument('--resources', nargs='+', type=str, help='Resources to scan')
+
+    return parser.parse_args()
+
+
+def scan_resource(resource, target):
+    global passed
+    res = requests.get(target, timeout=20, allow_redirects=False)
+    res_head = str(res.headers.get('Access-Control-Allow-Origin'))
+    print("Allowed Origins: " + res_head)
+    loc_passed = res_head == resource
+    if passed:
+        passed = loc_passed
+    return loc_passed
+
+
+if __name__ == '__main__':
+    passed = True
+    args = parse_args()
+    if not args.resources or args.type != 'api':
+        print("Skipping cors policy test")
+        sys.exit(0)
+    for resource in args.resources:
+        print(str(resource) + (' \033[92mpassed\033[0m' if scan_resource(str(resource), args.domain) else ' \033[91mfailed\033[0m'))
+    sys.exit(0 if passed else 1)
