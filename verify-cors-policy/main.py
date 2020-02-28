d16372cce93cf0e818722e38773bce5a7c7156c1
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-02-28 13:05:09 +0100

    Simplified policy test DAT-2065

--- verify-cors-policy/main.py
+++ verify-cors-policy/main.py
@@ -6,28 +6,23 @@ import sys
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('domain', type=str, help='Secured API url')
-    parser.add_argument('type', type=str, help='type')
-    parser.add_argument('resource', type=str, help='Resources to scan')
 
     return parser.parse_args()
 
 
-def scan_resource(resource, target):
-    global passed
-    res = requests.get('https://{}'.format(target), timeout=20, allow_redirects=False)
-    res_head = str(res.headers.get('Access-Control-Allow-Origin'))
-    print("Allowed Origins: " + res_head)
-    loc_passed = res_head == resource
-    if passed:
-        passed = loc_passed
-    return loc_passed
+def scan(arguments):
+    res = requests.get('https://{}'.format(arguments.domain), timeout=20, allow_redirects=False)
+    res_head = res.headers.get('Access-Control-Allow-Origin')
+
+    if not res_head:
+        print("Failing policy test: No Allowed Origins Specified")
+        return False
+
+    print("Passing policy test: Allowed Origins Specified")
+    return True
 
 
 if __name__ == '__main__':
-    passed = True
     args = parse_args()
-    if not args.resource or args.type != 'api':
-        print("Skipping cors policy test")
-        sys.exit(0)
-    print(str(args.resource) + (' \033[92mpassed\033[0m' if scan_resource(str(args.resource), args.domain) else ' \033[91mfailed\033[0m'))
-    sys.exit(0 if passed else 1)
+
+    sys.exit(0 if scan(args) else 1)
