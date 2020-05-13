4a82c63c02d10d3e72a14fa634e029cacd615430
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-05-13 16:33:23 +0200

    Increased timeout DAT-2721

--- verify-no-ssl/ssl3.sh
+++ verify-no-ssl/ssl3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssl=$(timeout 5 /usr/local/bin/openssl s_client -connect "$1":443 -ssl3)
+ssl=$(timeout 15 /usr/local/bin/openssl s_client -connect "$1":443 -ssl3)
 echo $?
 echo "$ssl"
 if [[ $ssl == *"Verify return code: 0 (ok)"* ]]; then
