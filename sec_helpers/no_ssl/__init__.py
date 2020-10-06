e157d244df2137f3a076d221254b525279a0ca67
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-10-06 10:44:56 +0200

    Change to ssl3 version check DAT-4269

--- sec_helpers/no_ssl/__init__.py
+++ sec_helpers/no_ssl/__init__.py
@@ -1,4 +1,3 @@
-import ssl
 import re
 import subprocess  # nosec
 from os import path
@@ -8,14 +7,16 @@ import sys
 class NoSsl:
 
     def __init__(self, domain):
+        ossl_version = \
+            str(subprocess.check_output(['openssl', 'version']))  # nosec
         if str(re.findall('([0-9]\\.[0-9]\\.[0-9]).',
-                          ssl.OPENSSL_VERSION)[0]) != '1.0.2':
+                          ossl_version)[0]) != '1.0.2':
             print(
                 '{} is not the right version. Download a compatible version'
                 '(1.0.2): '
                 'https://www.openssl.org/source/old/1.0.2'.format(
-                    ssl.OPENSSL_VERSION))
+                    ossl_version))
         elif subprocess.call(['bash',  # nosec
                               '{}/ssl3.sh'.format(path.dirname(  # nosec
-                                path.realpath(__file__))), domain]):
+                                  path.realpath(__file__))), domain]):
             sys.exit(1)
