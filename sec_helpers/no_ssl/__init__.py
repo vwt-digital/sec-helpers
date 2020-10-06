4ae7c59d297dfba9c4325cf3c57e5e7a33297baa
MaxvanderWeide <max.vanderweide@student.hu.nl>
2020-10-06 11:43:44 +0200

    Better ssl test DAT-4269

--- sec_helpers/no_ssl/__init__.py
+++ sec_helpers/no_ssl/__init__.py
@@ -1,12 +1,16 @@
 import re
 import subprocess  # nosec
-from os import path
 import sys
 
 
 class NoSsl:
 
     def __init__(self, domain):
+        self.domain = domain
+        if not self.test_process():
+            sys.exit(1)
+
+    def test_process(self):
         ossl_version = \
             str(subprocess.check_output(['openssl', 'version']))  # nosec
         if str(re.findall('([0-9]\\.[0-9]\\.[0-9]).',
@@ -16,7 +20,16 @@ class NoSsl:
                 '(1.0.2): '
                 'https://www.openssl.org/source/old/1.0.2'.format(
                     ossl_version))
-        elif subprocess.call(['bash',  # nosec
-                              '{}/ssl3.sh'.format(path.dirname(  # nosec
-                                  path.realpath(__file__))), domain]):
-            sys.exit(1)
+            return True
+        else:
+            try:
+                print(subprocess.check_output(  # nosec
+                    ['/usr/local/bin/openssl', 's_client',
+                     '-connect', '{}:443'.format(
+                        self.domain),
+                     '-ssl3'], timeout=15))
+            except subprocess.CalledProcessError:
+                print('No sign of SSLv3 allowance found')
+                return True
+            print('SSLv3 seems to be allowed')
+            return False
